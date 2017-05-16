f38671cef5a5bd7be88b6bbb70b04532c36edb96
StivenPeter <stivenpeter@gmail.com>
2017-05-16 12:33:03 -0400

    Update clubInfo.py

--- utils/clubInfo.py
+++ utils/clubInfo.py
@@ -3,11 +3,32 @@ import sqlite3
 import os
 
 def addentry(usern, email, advisor, advisorEmail):
-  f = "data/data.db"
+	f = "data/data.db"
 	db = sqlite3.connect(f)
-  sp = db.cursor()
-  insert = "INSERT INTO users VALUES ('%s', '%s', '%s', '%s')"%(usern, email, advisor, advisorEmail) 
+  	sp = db.cursor()
+  	insert = "INSERT INTO users VALUES ('%s', '%s', '%s', '%s')"%(usern, email, advisor, advisorEmail) 
 	sp.execute(insert)
 	db.commit()
 	db.close()
-  return True
+  	return True
+def getData(username):
+	f = "data/data.db"
+	db = sqlite3.connect(f)
+	c = db.cursor()
+	checkUser = "SELECT * FROM users WHERE usern==?"
+	c.execute(checkUser, (username,))
+	r = c.fetchone()
+	db.commit()
+	db.close()
+	return r
+def getName(username):
+	return getdata(Username)[0] 
+
+def getEmail(username):
+	return getdata(Username)[1] 
+
+def getAdvisor(username):
+	return getdata(Username)[2] 
+
+def getAdvisorEmail(username):
+	return getdata(Username)[3] 
