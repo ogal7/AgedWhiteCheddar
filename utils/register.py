4f3619b9f5bceb6d3e62cdbfc329239b5120be3b
gTopa <gtopa@stuy.edu>
2017-05-21 19:32:16 -0400

    work

--- utils/register.py
+++ utils/register.py
@@ -1,29 +1,48 @@
 import hashlib
 import sqlite3
 import os
+import random
 
 def addCode(email):
-    f = "data/data.db"
+    f = "../data/data.db"
     db = sqlite3.connect(f)
     og = db.cursor()
 
-    request="SELECT code from accounts"
-    codes=og.execute(request)
-    code=os.random(32)
-    while(code in codes):
-        code=os.random(32)
+    code=random.randint(1000000000,9999999999)
+    print code
+    while(badCode(code)):
+        code=random.randint(1000000000,9999999999)
 
     insert="INSERT INTO accounts VALUES ('%s', '%s')"%(email, code)
     og.execute(insert)
     db.commit()
     db.close()
 
+def badCode(code):
+    f = "../data/data.db"
+    db = sqlite3.connect(f)
+    og = db.cursor()
+
+    request="SELECT code from accounts"
+    codes=og.execute(request)
+    for thing in codes:
+        if thing[0]==codes:
+            return false
+
+    db.close()
+    
+    
 def checkCode(email, code):
     L=[email, code]
-    f = "data/data.db"
+    f = "../data/data.db"
     db = sqlite3.connect(f)
     og = db.cursor()
 
     request="SELECT * from accounts"
     codes=og.execute(request)
-    return (L in codes)
+    for things in codes:
+        if((email, code) == things):
+            return True
+
+addCode('lol')
+print checkCode('lol', '8002752771')
