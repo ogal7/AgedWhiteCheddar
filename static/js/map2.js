82bc59f9622569c3f29d86ee6bcee01d9077fbd6
ogal7 <ogallager@stuy.edu>
2017-05-24 12:28:50 -0400

    ugh

--- static/js/map2.js
+++ static/js/map2.js
@@ -2,6 +2,6 @@ var container = document.getElementById("vimage");
 var x = container.getAttribute("width")/2;
 var y = container.getAttribute("height")/2;
 
-
+buttonDiv = document.getElementById("roomButtons");
 	
 
--- static/js/map3.js
+++ static/js/map3.js
@@ -1,3 +1,6 @@
 var container = document.getElementById("vimage");
 var x = container.getAttribute("width")/2;
 var y = container.getAttribute("height")/2;
+
+buttonDiv = document.getElementById("roomButtons");
+
