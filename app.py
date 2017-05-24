237b66e1c9501616b84255b5aa11de136e76d110
Brian Yang <brian.yang328@gmail.com>
2017-05-23 21:37:21 -0400

    Add more comments

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, request, session, redirect, url_for, Response
 import hashlib
 import time
-from utils import users, clubInfo, accounts
+from utils import users, club, room, reserve, email
 
 app = Flask(__name__)
 app.secret_key = 'agedwhitecheddar'
 
+# =====================
+# main page
+# =====================
 @app.route('/', methods = ["GET", "POST"])
 @app.route('/home/', methods = ["GET", "POST"])
 def main():
     if 'user' in session:
         return redirect(url_for("homepage"))
-        #pass
     return render_template("entry.html")
 
+# =====================
+# authentication
+# =====================
 @app.route("/auth/", methods = ["POST"])
 def auth():
     loginResponse = request.form
@@ -23,8 +28,8 @@ def auth():
     print formMethod
 
     if formMethod == "login":
-        if users.checkLogin(username,password) == True:
-            session['user']= username
+        if users.checkLogin(username, password):
+            session['user'] = username
             return redirect(url_for("homepage"))
         else:
             message = "login failed"
@@ -34,9 +39,9 @@ def auth():
 
     if formMethod == "register":
         code = loginResponse['code']
-        if users.checkRegister(username, code) == True: #code/user match is valid
+        if users.checkRegister(username, code): #code/user match is valid
             print "hi"
-            if users.isStudent(code) == True:
+            if users.isStudent(code):
                 #print "student"
                 users.createAccount(username,password,code)
                 session['user'] = username
@@ -50,16 +55,9 @@ def auth():
 
     return redirect(url_for("main"))
 
-# work on fixing register
-@app.route("/register/", methods=["POST", "GET"])
-def register():
-    if "code" in request.form:
-        code = request.form["code"]
-        if (register.checkCode(code)):
-            pass
-        #add user/pass/clubname to table
-    return render_template("clubRegister.html")
-
+# =====================
+# logged in users
+# =====================
 @app.route("/homepage/", methods=["POST","GET"])
 def homepage():
     return render_template("homepage.html")
@@ -89,20 +87,26 @@ def date(date):
             return render_template("map4.html")
     return render_template("floors.html", message=date)
 
-@app.route("/logOut/")
-def logout():
-    if 'user' in session:
-        session.pop("user")
-    return redirect(url_for("main"))
+@app.route('/floors/')
+def choose_floor():
+    pass
 
 @app.route('/settings/')
 def settings():
     pass
 
-@app.route('/floors/')
-def choose_floor():
-    pass
+# =====================
+# log out
+# =====================
+@app.route("/logout/")
+def logout():
+    if 'user' in session:
+        session.pop("user")
+    return redirect(url_for("main"))
 
+# =====================
+# run app
+# =====================
 if __name__ == '__main__':
     app.debug = True
     app.run(threaded = True)
