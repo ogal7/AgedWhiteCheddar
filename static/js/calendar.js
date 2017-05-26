a25708004be888ed5ff3e61b32b11969d666c323
Brian Yang <brian.yang328@gmail.com>
2017-05-26 12:23:19 -0400

    Fix calendar

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,64 +1,14 @@
-/*
-cal = document.getElementById("cal")
-month = document.getElementById("month")//table header element
-week1 = document.getElementById("1stWeek");
-week2 = document.getElementById("2ndWeek");
-week3 = document.getElementById("3rdWeek");
-week4 = document.getElementById("4thWeek");
-week5 = document.getElementById("5thWeek");
-var weeks = [ week1, week2, week3, week4, week5];
-var todaysCalendar = function () {
-	var d = new Date();
-	dayNum = d.getDate()//returns day of the month (1-31)
-	dayWeek = d.getDay()//returns day of the week (0-6)
-	year = d.getFullYear()//gets the year
-	month1 = d.getMonth()// returns month from 0-11
-	month.innerHTML = getMonthName(month1);
-	days = getDaysInMonth(month1, year);
-	console.log(days);
-	//listOfWeeks = getWholeMonth(month1,year);
-	//console.log(listOfWeeks);
-}
-function getDaysInMonth(month, year) {
-     var date = new Date(year, month, 1);
-     var days = [];
-     //console.log();
-     while (date.getMonth() === month) {
-        days.push(new Date(date));
-        date.setDate(date.getDate() + 1);
-     }
-     //console.log(days);
-     return days;
-}
-var updateCalendar = function () {
-	//what is called when the arrows are pushed
-}
-var checkDate = function() {
-	//what is called when a date is clicked
-}
-var getWholeMonth = function(month,year) {
-	var mon = [
-		[0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0]
-	]
-	//console.log(mon);
-	var listOfDays = getDaysInMonth(month, year);
-	var j = 0;
-	for (list in mon){
-		for ( i=0; i < 7; i++) {
-			//console.log(list[i]);
-			console.log(listOfDays[j]);
-			list[i] = listOfDays[j];
-			//console.log(list[i]);
-			j++ ;
-		}
-	}
-	//console.log(mon);
-	return mon;
-}
+// the previously commented out calendar javascript
+// is in calendar.js.old
+
+// =======================================
+// DATE VARIABLE
+// =======================================
+var d = new Date();
+
+// =======================================
+// FUNCTIONS
+// =======================================
 function getMonthName(month) {
     var monthName = "";
     switch(month){
@@ -100,185 +50,148 @@ function getMonthName(month) {
 	break;
     }
     return monthName;
-}
-todaysCalendar();*/
-
-function getMonthName(month) {
-    var monthName = "";
-    switch(month){
-    case 0:
-	monthName = "January";
-	break;
-    case 1:
-	monthName = "February";
-	break;
-    case 2:
-	monthName = "March";
-	break;
-    case 3:
-	monthName = "April";
-	break;
-    case 4:
-	monthName = "May";
-	break;
-    case 5:
-	monthName = "June";
-	break;
-    case 6:
-	monthName = "July";
-	break;
-    case 7:
-	monthName = "August";
-	break;
-    case 8:
-	monthName = "September";
-	break;
-    case 9:
-	monthName = "October";
-	break;
-    case 10:
-	monthName = "November";
-	break;
-    case 11:
-	monthName = "December";
-	break;
-    }
-    return monthName;
-}
-
-function fixDays(monthNum){
-    if(monthNum == 1){
-	$('.days li').each(function(){
-	    if($(this).text() >= 29){
-		$(this).hide();
-	    } 
-	});
-    } else if(monthNum == 3 || monthNum == 5 || monthNum == 8 || monthNum == 10){
-	$('.days li').each(function(){
-	    if($(this).text() == 31){
-		$(this).hide();
-	    } 
-	    if($(this).text() == 29 || $(this).text() == 30){
-		$(this).show();
-	    } 
-	});
-    } else {
-	$('.days li').each(function(){
-	    if($(this).text() >= 29){
-		$(this).show();
-	    } 
-	});
-    }
 }
 
 function weekdays() {
     var dayTags = "<li>Su</li>\n" +
-	    "<li>Mo</li>\n" +
-	    "<li>Tu</li>\n" +
-	    "<li>We</li>\n" + 
-	    "<li>Th</li>\n" +
-	    "<li>Fr</li>\n" +
-            "<li>Sa</li>\n"
+	"<li>Mo</li>\n" +
+	"<li>Tu</li>\n" +
+	"<li>We</li>\n" +
+	"<li>Th</li>\n" +
+	"<li>Fr</li>\n" +
+        "<li>Sa</li>\n"
     return dayTags;
 }
+
+// function weekends(startDay, monthLength) {
+//     var weekendList = []
+//     if (startDay == 0){
+// 	var i = 1;
+// 	while (i <= monthLength) {
+// 	    weekendList.push(i);
+// 	    i += 6;
+// 	}
+// 	i = 8;
+// 	while (i <= monthLength) {
+// 	    weekendList.push(i)
+// 	    i+=6;
+// 	}
+// 	return weekendList; //list of days that are weekends
+//     }
+//     else {
+// 	var i = 7 - startDay;
+// 	while (i <= monthLength) {
+// 	    weekendList.push(i);
+// 	    i+=7;
+// 	}
+// 	var i = 8 - startDay;
+// 	while (i <= monthLength) {
+// 	    weekendList.push(i);
+// 	}
+// 	return weekendList;
+//     }
+// }
+
 
 function numDays(year, month) {
     var numTags = "";
     var firstDay = new Date(year, month, 1);
+    var daysInMonth = new Date(year, month + 1, 0).getDate();
     // getDay() returns 0 if the day is Sunday, so we make sure that
     // we do not prepend a placeholder if the day is Sunday
     // Otherwise, there would be an extra <li> prepended, messing up
     // the layout of the calendar
-    if (firstDay.getDay() != 0) {
-	for(i = 0; i < firstDay.getDay(); i++) { 
+    dayNum = firstDay.getDay();
+    // redList = weekends(dayNum, daysInMonth);
+    if (dayNum != 0) {
+	for(i = 0; i < dayNum; i++) {
 	    numTags += "<li></li>\n";
 	}
     }
-    for (i = 1; i <= 31; i++) {
+    for (i = 1; i <= daysInMonth; i++) {
+    	// if (i in redList) {
+	//     numTags += "<li id=w>" + i.toString() + "</li>\n "
+	//     dayNum++;
+	// } else {}
 	numTags += "<li>" + i.toString() + "</li>\n "
+	dayNum++;
+	if (dayNum == 7) {
+	    numTags += "<br/>";
+	    dayNum = 0;
+	}
     }
     return numTags;
 }
 
-function displayDays(year, month) {    
+function displayDays(year, month) {
     $('.weekdays').append(weekdays());
     $('.days').append(numDays(year, month));
 }
 
-function activeDay(date, month, year) {
-	console.log(year);
-	//var month = date.getMonth();
+function manageDates(date, month, year) {
+    // var daysInMonth = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
+
     $('.days li').each(function(){
 	
-	//console.log(parseInt($(this).text()));
-	//console.log(parseInt(date) + 14);
-	if (  parseInt($(this).text()) <= parseInt(date) + 14 && parseInt($(this).text()) >= parseInt(date)) {
-		$(this).html('<font color = green> <a href=' + month.toString() + "-" + $(this).text() + "-" + year.toString() + '>' + $(this).text() + '</a> </font>\n')
-		//console.log('<a href=' + month.toString() + "-" + $(this).text() + "-" + year.toString() + '>');
-		//<a href=4- 27  -2017>
+	//if ( $(this).id == "w" ) {
+	//	$(this).html('<font-color:#D63E3E>' +  $(this).text() + '</font>\n')
+	//}
+	if( parseInt($(this).text()) <= parseInt(date) + 14 &&
+	    parseInt($(this).text()) >= parseInt(date) ) {
+	    $(this).html('<a style="color:#2ba6cb;" href=' + month.toString() + "-" +
+			 $(this).text() + "-" + year.toString() + '>' + $(this).text() + '</a> </font>\n')
 	}
-	if ($(this).text() == date){
+
+	if ($(this).text() == d.getDate() && month === d.getMonth()) {
 	    $(this).html('<span class = "active"> <a href=' + month.toString() + "-" + date + "-" + year.toString() + '>' + date + '</a> </span>\n')
 	}
+
+
     });
 }
 
-function events(date) {
-	li = document.getElementsByClassName("days")[0].childNodes;
-	for (i in li) {
-		if (i in document.getElementsByTagName("li")) {
-			i.addEventListener("click", launch);
-		}
-	}
-}
-
-var launch = function(e) {
-	console.log("hi")
-}
-
+// =======================================
+// ON DOCUMENT READY
+// =======================================
 
 $(document).ready(function(){
-    var d = new Date();
-    var monthNum = d.getMonth();
-    var currentYear = d.getFullYear();
-    var currentDate = d.getDate();
+    var monthNum = parseInt(d.getMonth());
+    var currentYear = parseInt(d.getFullYear());
+    var currentDate = parseInt(d.getDate());
     var currentMonth = getMonthName(monthNum);
     $('.m').html(currentMonth);
     $('.y').html(currentYear);
     displayDays(currentYear, monthNum);
-    fixDays(monthNum);
-    activeDay(currentDate, monthNum, currentYear);
-    //events(currentDate);
+    manageDates(currentDate, monthNum, currentYear);
 
-    $(document).on("click", ".prev", function(){
-	$('.weekdays').empty();
-	$('.days').empty();
-	monthNum -= 1;
-	if (monthNum == -1) {
-	    monthNum = 11;
-	    currentYear -= 1;
-	}
-	displayDays(currentYear, monthNum);
-	activeDay(currentDate);
-	currentMonth = getMonthName(monthNum);
-	fixDays(monthNum);
-	$('.m').html(currentMonth);
-	$('.y').html(currentYear);
-    });
+    // $(".prev").on("click", function(){
+    // 	console.log("what");
+    // 	$('.weekdays').empty();
+    // 	$('.days').empty();
+    // 	monthNum -= 1;
+    // 	if (monthNum == -1) {
+    // 	    monthNum = 11;
+    // 	    currentYear -= 1;
+    // 	}
+    // 	displayDays(currentYear, monthNum);
+    // 	manageDates(currentDate, monthNum, currentYear);
+    // 	currentMonth = getMonthName(monthNum);
+    // 	$('.m').html(currentMonth);
+    // 	$('.y').html(currentYear);
+    // });
 
-    $(document).on("click", ".next", function() {
-	$('.weekdays').empty();
-	$('.days').empty();
-	monthNum += 1;
-	if (monthNum == 12) {
-	    monthNum = 0;
-	    currentYear += 1;
-	}
-	displayDays(currentYear, monthNum);
-	activeDay(currentDate);
-	currentMonth = getMonthName(monthNum);
-	fixDays(monthNum);
-	$('.m').html(currentMonth);
-	$('.y').html(currentYear);
-    });
+    // $(".next").on("click", function() {
+    // 	$('.weekdays').empty();
+    // 	$('.days').empty();
+    // 	monthNum += 1;
+    // 	if (monthNum == 12) {
+    // 	    monthNum = 0;
+    // 	    currentYear += 1;
+    // 	}
+    // 	displayDays(currentYear, monthNum);
+    // 	manageDates(currentDate, monthNum, currentYear);
+    // 	currentMonth = getMonthName(monthNum);
+    // 	$('.m').html(currentMonth);
+    // 	$('.y').html(currentYear);
+    // });
 });
