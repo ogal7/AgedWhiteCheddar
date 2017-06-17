2bbb9acf334407e29157bb6d08f432f8bd99be7f
Brian Yang <brian.yang328@gmail.com>
2017-06-17 15:27:21 -0400

    Remove other code

--- surooms/static/js/map_base.js
+++ surooms/static/js/map_base.js
@@ -14,35 +14,3 @@ console.log(date)
 var actionLink = "/reserve/" + date + "/"
 whichRoom.setAttribute("action", actionLink);
 console.log(actionLink);
-
-// function makeRed(redRooms) {
-//     var toRed = redRooms.split(",");
-
-//     for (i = 0; i < toRed.length; i++) {
-// 	if (toRed[i] !== "") {
-// 	    toRed[i] = parseInt(toRed[i]);
-// 	} else {
-// 	    toRed.splice(i, 1);
-// 	}
-//     }
-
-//     rooms = document.getElementsByTagName("rect");
-//     for (i = 0; i < rooms.length; i++) {
-// 	if (toRed.indexOf(parseInt(rooms[i].getAttribute("id"))) > -1) {
-// 	    var replaceIndex = rooms[i].getAttribute("style").indexOf(";");
-// 	    console.log(replaceIndex);
-// 	    rooms[i].setAttribute("style", "fill:pink" + rooms[i].getAttribute("style").substring(replaceIndex));
-// 	}
-//     }
-// }
-
-// function loadDoc() {
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-// 	if (this.readyState == 4 && this.status == 200) {
-//     	    makeRed(this.responseText);
-// 	}
-//     };
-//     xhttp.open("GET", "/redRooms/" + date + "/", true);
-//     xhttp.send();
-// }
